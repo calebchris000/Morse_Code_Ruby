b93c41261618b67faacd442243e16d78f5e661aa
Caleb Nwaizu <caleb.nwaizu@gurugeeksroyalty.biz>
2023-05-11 11:51:18 +0100

    Resolve requirements

--- morse_decode.rb
+++ morse_decode.rb
@@ -1,13 +1,29 @@
-def decode(morse)
-  morse_alphabet = {
-    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G',
-    '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N',
-    '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U',
-    '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z', '.----.' => "'", ' ' => '',
-    '   ' => ' '
-  }
-  decoded_message = morse.split(/(\s{1,})/).map { |char| morse_alphabet[char] }.join
-  print decoded_message
+# frozen_string_literal: true
+
+MORSE_ALPHABET = {
+  '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G',
+  '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N',
+  '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U',
+  '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z', '.----.' => "'", ' ' => '',
+  '   ' => ' '
+}.freeze
+
+def decode_char(char)
+  MORSE_ALPHABET[char]
 end
 
-decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
+def decode_word(word)
+  decoded = ''
+  word.split(/(\s{1,})/).each { |n| decoded += decode_char(n) }
+  puts "#{decoded}\n"
+end
+
+def decode_sentence(sentence)
+  decoded_sentence = sentence.split(/(\s{1,})/)
+  message = ''
+  decoded_sentence.each { |n| message += decode_char(n) }
+  puts message
+end
+puts decode_char('.-')
+decode_word('-- -.--')
+decode_sentence('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
